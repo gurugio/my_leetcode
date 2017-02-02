945d3a1b918b94f6b5d5b528c9d0d00c0c74bfe9
gurugio <gurugio@hanmail.net>
2017-02-02 23:35:03 +0100

    206. Reverse Linked List
    206. Reverse Linked List

--- easy.c
+++ easy.c
@@ -1,4 +1,32 @@
 /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode *prev, *cur, *next;
+
+    if (head == NULL)
+        return NULL;
+    if (head->next == NULL)
+        return head;
+
+    
+    prev = head;
+    cur = head->next;
+    head->next = NULL;
+    while (cur != NULL) {
+        next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    }
+    return prev;
+}
+
+/**
  * Old version is return *(int *)a - *(int *)b
  * but it's buggy because it cannot sort -2147483648 (-2147483648 - 1 = 2147483647)
  */
