d919f7d46963ceaab55dfb45f33205ea83384e65
Gioh Kim <gurugio@hanmail.net>
2018-07-05 22:36:43 +0200

    cracking the coding interview: ex2.2, ex2.5

--- ctci_02.c
+++ ctci_02.c
@@ -109,9 +109,80 @@ void ex_2_1(void)
 		printf("after-list: %d\n", n->val);
 }
 
+int list_nth_back(struct list_node *n, int k)
+{
+	int t;
+	if (n->next == NULL)
+		return 1;
+	t = list_nth_back(n->next, k) + 1;
+	if (t == k)
+		printf("n-th=%d\n", n->val);
+	return t;
+}
+
+struct list_node *list_add_val(struct list_node *n1,
+							   struct list_node *n2)
+{
+	int carry = 0;
+	struct list_node *cur = NULL, *ret;
+	struct list_node *na, *nb;
+	
+	for (na = n1, nb = n2; na && nb; na = na->next, nb = nb->next) {
+		if (cur) {
+			cur->next = calloc(1, sizeof(*cur));
+			cur = cur->next;
+		} else {
+			cur = calloc(1, sizeof(*cur));
+			ret = cur;
+		}
+		cur->val = (na->val + nb->val + carry) % 10;
+		carry = (na->val + nb->val + carry) / 10;
+	}
+	for (; na; na = na->next) {
+		cur->next = calloc(1, sizeof(*cur));
+		cur = cur->next;
+		cur->val = (na->val + carry) % 10;
+		carry = (na->val + carry) / 10;
+	}
+	for (; nb; nb = nb->next) {
+		cur->next = calloc(1, sizeof(*cur));
+		cur = cur->next;
+		cur->val = (nb->val + carry) % 10;
+		carry = (nb->val + carry) / 10;
+	}
+	return ret;
+}
+
+void list_print(struct list_node *n)
+{
+	int i;
+	for (; n; n=n->next)
+		printf("%2d ", n->val);
+	printf("\n");
+}
+
 int main(void)
 {
+	int i;
+	srand(time(NULL));
+
 	ex_2_1();
+
+	struct list_node *head = calloc(1, sizeof(*head));
+	head->val = rand() % 10;
+	for (i = 0; i < 5; i++) {
+		list_insert(head, rand() % 10);
+	}
+	list_nth_back(head, 3); // ex2.2
+
+	struct list_node *head2 = calloc(1, sizeof(*head2));
+	head2->val = rand() % 10;
+	for (i = 0; i < 10; i++) {
+		list_insert(head2, rand() % 10);
+	}
+	list_print(head);
+	list_print(head2);
+	list_print(list_add_val(head, head2)); // ex2.5
 
 	return 0;
 }
