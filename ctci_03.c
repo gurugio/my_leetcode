ac740180f12a3d86dd65f6354db3ed8a26551874
Gioh Kim <gi-oh.kim@profitbricks.com>
2018-07-09 18:07:06 +0200

    cracking the coding interview: ex 3.2

--- ctci_03.c
+++ ctci_03.c
@@ -4,11 +4,13 @@
 
 struct stack_node {
 	int val;
+	struct stack_node *min_next; // ex3.2
 	struct stack_node *next;
 };
 
 struct stack {
 	struct stack_node *top;
+	struct stack_node *min_top; // ex3.2
 };
 
 struct queue_node {
@@ -26,13 +28,26 @@ void stack_push(struct stack *stack, int val)
 	n->val = val;
 	n->next = stack->top;
 	stack->top = n;
+	if (!stack->min_top || stack->min_top->val > val) {
+		n->min_next = stack->min_top;
+		stack->min_top = n;
+	}
 }
+
+int stack_min(struct stack *stack)
+{
+	return stack->min_top ? stack->min_top->val : -1;
+}
+	      
 
 int stack_pop(struct stack *stack)
 {
 	int ret = stack->top->val;
 	struct stack_node *n = stack->top;
 	stack->top = n->next;
+	if (n == stack->min_top) {
+		stack->min_top = n->min_next;
+	}
 	free(n);
 	return ret;
 }
@@ -79,7 +94,7 @@ int queue_empty(struct queue *queue)
 	return !queue->head;
 }
 
-int main(void)
+void basic_test(void)
 {
 	int i;
 	srand(time(NULL));
@@ -127,6 +142,34 @@ int main(void)
 		printf("%d ", queue_remove(q));
 	printf("\n");
 	printf("q-empty=%d\n", queue_empty(q));
-	
+}
+
+
+void test_ex_3_2(void)
+{
+	int i;
+	srand(time(NULL));
+	struct stack *s = calloc(1, sizeof(*s));
+
+	for (i = 0; i < 10; i++) {
+		int t = rand() % 100;
+		printf("%d ", t);
+		stack_push(s, t);
+
+		printf("min=%d\n", stack_min(s));
+	}
+	printf("\n");
+
+	printf("pop:  ");
+	for (i = 0; i < 10; i++) {
+		printf("%d ", stack_pop(s));
+		printf("min=%d\n", stack_min(s));
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	test_ex_3_2();
 	return 0;
 }
