92f28fae93d776febb4ab4e50311ac9324c33c6e
Gioh Kim <gi-oh.kim@cloud.ionos.com>
2019-11-06 10:20:59 +0100

    1021. Remove Outermost Parentheses

--- easy.py
+++ easy.py
@@ -1,3 +1,19 @@
+# 1021. Remove Outermost Parentheses
+class Solution:
+    def removeOuterParentheses(self, S: str) -> str:
+        counter = 0
+        ret = ""
+        for c in S:
+            if c == '(':
+                if counter != 0:
+                    ret += c
+                counter += 1
+            else:
+                counter -= 1
+                if counter != 0:
+                    ret += c
+        return ret
+
 # 1221. Split a String in Balanced Strings
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
