2ac0366f398e03a5013540a6bf64d83f274f7ea0
gurugio <gurugio@gmail.com>
2020-11-21 06:44:12 +0100

    16. 3Sum Closest
    16. 3Sum Closest

--- medium.py
+++ medium.py
@@ -1,3 +1,28 @@
+# 16. 3Sum Closest
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        closest = 99999
+
+        for i in range(len(nums) - 2):
+            left = i + 1
+            right = len(nums) - 1
+
+            while (left < right):
+                s = nums[i] + nums[left] + nums[right]
+                if abs(s - target) < abs(closest - target):
+                    closest = s
+                #print("i={} left={} right={} closest={} s={}".format(i, left, right, closest, s))
+
+                if s > target:
+                    right = right - 1
+                elif s < target:
+                    left = left + 1
+                else:
+                    return closest # same to target
+        return closest
+                    
+
 # 15. 3Sum
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
