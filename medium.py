eebb6eefe1869e93e3f95af94d7a0ffa18cd00a1
gurugio <gurugio@gmail.com>
2020-11-22 06:10:22 +0100

    18. 4Sum
    18. 4Sum

--- medium.py
+++ medium.py
@@ -1,3 +1,52 @@
+# 18. 4Sum
+class Solution:
+    def threeSum(self, nums: List[int], target) -> List[List[int]]:
+        ret = []
+        if len(nums) < 3:
+            return []
+
+        nums.sort()
+
+        for cur in range(len(nums) - 2):
+            left = cur + 1
+            right = len(nums) - 1
+
+            # skip duplicated numbers
+            if cur > 0 and nums[cur] == nums[cur - 1]:
+                continue
+        
+            while (left < right):
+                sum = nums[cur] + nums[left] + nums[right]
+                if sum > target:
+                    right = right - 1
+                elif sum < target:
+                    left = left + 1
+                else:
+                    ret.append([nums[cur], nums[left], nums[right]])
+
+                    # skip duplicated numbers
+                    while left < right and nums[left] == nums[left + 1]:
+                        left = left + 1
+                    left = left + 1
+                    while left < right and nums[right - 1] == nums[right]:
+                        right = right - 1
+                    right = right - 1
+        return ret
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        if len(nums) < 4:
+            return []
+        ret = []
+        nums.sort()
+        for cur in range(len(nums) - 3):
+            # no duplicated
+            if cur > 0 and nums[cur] == nums[cur - 1]:
+                continue
+
+            tmp = self.threeSum(nums[cur + 1:], target - nums[cur])
+            if tmp:
+                ret = ret + [[nums[cur]] + x for x in tmp]
+        return ret
+    
 # 16. 3Sum Closest
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
