0d89d4df94a3e712d2a1b893018a0ac0d9525513
Gioh Kim <gurugio@l.com>
2025-03-06 21:27:24 +0900

    baekjoon: 15650

---
+++ baekjoon/15650.py
@@ -0,0 +1,20 @@
+tmp = list(map(int, input().split()))
+m = tmp[0]
+n = tmp[1]
+
+nums = []
+for i in range(1, m + 1):
+    nums.append(i)
+
+def find(nums, ret, depth):
+    #print("===", nums, ret, depth)
+    if depth == 0:
+        for i in ret:
+            print(i, end=' ')
+        print()
+        return
+    for i in range(0, len(nums)):
+        if len(ret) == 0 or ret[-1] < nums[i]:
+            find(nums[:i] + nums[i+1:], ret + [nums[i]], depth - 1)
+
+find(nums, [], n)
